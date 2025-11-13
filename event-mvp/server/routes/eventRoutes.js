e934aef3d8404ee9423fd30a9e9ae1cbf409e6a0
Nicole Ariadna Celemin Triana <148727426+nicole1ariadna@users.noreply.github.com>
2025-11-13 14:42:10 -0500

    Delete event-mvp directory

--- event-mvp/server/routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getEvents, getEventById } = require('../controllers/eventController');
-
-router.get('/', getEvents);
-router.get('/:id', getEventById);
-
-module.exports = router;
